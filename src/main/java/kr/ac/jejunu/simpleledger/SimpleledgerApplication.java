363b2ed9789c857706f8e01b9c7b4c5fa50f7f0c
alzseven <jhp7140@gmail.com>
2021-06-06 16:20:02 +0900

    init

---
+++ src/main/java/kr/ac/jejunu/simpleledger/SimpleledgerApplication.java
@@ -0,0 +1,13 @@
+package kr.ac.jejunu.simpleledger;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleledgerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SimpleledgerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/kr/ac/jejunu/simpleledger/SimpleledgerApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.ac.jejunu.simpleledger;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleledgerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

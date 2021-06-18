4540a6054fca3b713a502c072c2e813ae550b712
alzseven <jhp7140@gmail.com>
2021-06-18 09:22:11 +0900

    Allow CORS PUT & DELETE

--- src/main/java/kr/ac/jejunu/simpleledger/security/config/WebMvcConfig.java
+++ src/main/java/kr/ac/jejunu/simpleledger/security/config/WebMvcConfig.java
@@ -3,6 +3,7 @@ package kr.ac.jejunu.simpleledger.security.config;
 import kr.ac.jejunu.simpleledger.security.AuthInterceptor;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -21,6 +22,10 @@ public class WebMvcConfig implements WebMvcConfigurer {
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**").allowedOrigins("*");
+        registry.addMapping("/**").allowedOrigins("*").allowedMethods(
+                HttpMethod.GET.name(),
+                HttpMethod.POST.name(),
+                HttpMethod.PUT.name(),
+                HttpMethod.DELETE.name());
     }
 }
